e976bf3c8471441fa20a1945aa58141f25a55412
Nikita Goldashevsky <nikitagoldashevsky@yandex.ru>
2025-01-07 17:08:18 +0300

    Make ball alternately change its move direction

--- ball.js
+++ ball.js
@@ -9,7 +9,10 @@ class Ball{
 
     draw(ctx) {
         ctx.beginPath()
-        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+        
+        const y = this.track.center.y - Math.abs(this.center.y - this.track.center.y)
+        ctx.arc(this.center.x, y, this.radius, 0, Math.PI * 2)
+
         ctx.strokeStyle = "white"
         ctx.stroke()
     }
