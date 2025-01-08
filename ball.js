3464d803c07d0ed19abbabf66b3964a48a570258
Nikita Goldashevsky <nikitagoldashevsky@yandex.ru>
2025-01-08 17:09:00 +0300

    Refactor Ball move direction handling and add playing sound when it changes its direction

--- ball.js
+++ ball.js
@@ -4,21 +4,23 @@ class Ball{
         this.radius = radius
         this.speed = speed
         this.offset = 0
+        this.direction = 1
         this.center = this.track.getPosition(this.offset)
     }
 
     draw(ctx) {
         ctx.beginPath()
-        
-        const y = this.track.center.y - Math.abs(this.center.y - this.track.center.y)
-        ctx.arc(this.center.x, y, this.radius, 0, Math.PI * 2)
-
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
         ctx.strokeStyle = "white"
         ctx.stroke()
     }
 
     move() {
-        this.offset += this.speed
+        this.offset += this.speed * this.direction
         this.center = this.track.getPosition(this.offset)
+        if (this.center.y > this.track.center.y) {
+            this.direction *= -1
+            playSound()
+        }
     }
 }
