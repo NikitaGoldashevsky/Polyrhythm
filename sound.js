6f3cb6245c652d46067fa8b69dfb8c423286a979
Nikita Goldashevsky <nikitagoldashevsky@yandex.ru>
2025-01-09 17:09:42 +0300

    Increase sound playing duration

--- sound.js
+++ sound.js
@@ -1,6 +1,6 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 
-function playSound(frequency = 440, duration = 1) {
+function playSound(frequency = 440, duration = 2) {
     const osc = audioCtx.createOscillator()
     const envelope = audioCtx.createGain()
     osc.connect(envelope)
